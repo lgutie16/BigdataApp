62dc0b87d1664546adb4f7c40e104074182ba430
lgutie16 <lgutie16.eafit.edu.co>
2017-02-02 07:09:08 -0500

    readme template

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/user');
+
+var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err,
+            title: 'error'
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {},
+        title: 'error'
+    });
+});
+
+app.listen(3002,function(){
+    console.log("Escuchano por el puerto 3002")
+})
+
+
