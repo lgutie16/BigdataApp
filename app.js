283daea9738dacb9e6bd026016419e9ac93b5c26
lgutie16 <lgutie16.eafit.edu.co>
2017-02-04 18:55:49 -0500

    Fixing app

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express 		= require('express');
 var logger 			= require('morgan');
 var cookieParser 	= require('cookie-parser');
@@ -10,20 +9,10 @@ var teachers 		= require('./routes/teacher');
 var courses 		= require('./routes/course');
 var classes 		= require('./routes/class');
 var model 			= require('./models')
-
-
-var app = express();
-=======
-var express = require('express');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-var users = require('./routes/user');
+var cool = require('cool-ascii-faces');
 
 var app = express();
 
->>>>>>> 390897ce08828f13654d0b8425c8c0a55d34315c
 var env = process.env.NODE_ENV || 'development';
 
 
@@ -33,7 +22,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(cookieParser());
-<<<<<<< HEAD
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
@@ -44,50 +33,20 @@ app.use('/course', courses);
 app.use('/class', classes);
 
 
-=======
-
-app.use('/', routes);
-app.use('/users', users);
->>>>>>> 390897ce08828f13654d0b8425c8c0a55d34315c
 
 /// error handlers
 
 // development error handler
 // will print stacktrace
 
-<<<<<<< HEAD
+
 
 app.listen(3002,function(){
     console.log("Escuchano por el puerto 3002")
     model.sequelize.sync().then(function(){
         console.log("sequelize")
     })
-=======
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err,
-            title: 'error'
-        });
-    });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-    });
-});
-
-app.listen(3002,function(){
-    console.log("Escuchano por el puerto 3002")
->>>>>>> 390897ce08828f13654d0b8425c8c0a55d34315c
 })
 
 
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,10 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-<<<<<<< HEAD
 	data = { "nombre": {
 		"mio": "Linda"
 	}};
     res.render('../app/views/', data);
-=======
-  res.send('Estoy arriba');
->>>>>>> 390897ce08828f13654d0b8425c8c0a55d34315c
 });
 
 module.exports = router;
