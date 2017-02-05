78eab14785d2b6bea222a60071053449b6692cee
lgutie16 <lgutie16.eafit.edu.co>
2017-02-05 10:06:54 -0500

    Index and models

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ var sequelize = new Sequelize(config.db.database, config.db.username, config.db.
 var db        = {};
 
 
-
+/*
 fs
   .readdirSync(__dirname) //scan all the files in the given folder (__direname = currently folder)
   .filter(function(file) { // this filter takes all the js files (all the models)
@@ -28,3 +28,29 @@ module.exports = lodash.extend({
   sequelize: sequelize,
   Sequelize: Sequelize
 }, db);
+*/
+ if (process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
+  } else {
+    // the application is executed on the local machine ... use mysql
+    sequelize = new Sequelize('telematicadv', 'root', 'root')
+  }
+
+  global.db = {
+    Sequelize: Sequelize,
+    sequelize: sequelize,
+    Students:         sequelize.import(__dirname + '/studentModel') 
+    Teachers:         sequelize.import(__dirname + '/teacherModel')
+    Course:           sequelize.import(__dirname + '/courseModel')
+    SchoolClass:      sequelize.import(__dirname + '/classModel')
+  }
+}
+
+module.exports = global.db
