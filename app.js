77a0c36ed1c5977b49186319cd6fac1aa6e3ca24
lgutie16 <lgutie16.eafit.edu.co>
2017-02-04 21:34:23 -0500

    static route

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(cookieParser());
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/app'));
 app.set('view engine', 'ejs');
 
 app.use('/', routes);
