56ffe3d0dce1c0ad2ab4497df84a7561eec6385e
lgutie16 <lgutie16.eafit.edu.co>
2017-03-15 00:12:25 -0500

    Starting cache working -  Setup

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
-var express 		  = require('express');
-var logger 			  = require('morgan');
-var cookieParser 	= require('cookie-parser');
-var bodyParser 		= require('body-parser');
+var express 		  	= require('express');
+var logger 			  	= require('morgan');
+var cookieParser 		= require('cookie-parser');
+var bodyParser 			= require('body-parser');
 var path 			    = require("path");
 var cool 			    = require('cool-ascii-faces');
 var pg 				    = require('pg');
-var methodOverride  = require('method-override')
-var login 			 = require('./routes/index');
-var dashboard    = require('./routes/dashboard')
-var students 		 = require('./routes/student');
-var teachers 		 = require('./routes/teacher');
-var courses 		 = require('./routes/course');
-var classes 		 = require('./routes/class');
-var model 			 = require('./models')
+var methodOverride  	= require('method-override')
+var cache 				= require('memory-cache');
+var login 				= require('./routes/index');
+var dashboard    		= require('./routes/dashboard')
+var students 		 	= require('./routes/student');
+var teachers 		 	= require('./routes/teacher');
+var courses 		 	= require('./routes/course');
+var classes 		 	= require('./routes/class');
+var model 			 	= require('./models')
+
+var redis  = require("redis"),
+    client = redis.createClient();
 
 
-var app = express();
+
+var app 	= express();
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+ 
 
 var env = process.env.NODE_ENV || 'development';
-
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -39,15 +48,29 @@ app.use('/teacher', teachers);
 app.use('/course', courses);
 app.use('/class', classes);
 
-/// error handlers
 
-// development error handler
-// will print stacktrace
+// now just use the cache
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
 
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+    client.quit();
+});
+//End use cache
+
+//Start tests to fetch data and put it in cache
 
 
 app.listen(process.env.PORT || 8888,function(){
-    console.log("Escuchano por el puerto 3002")
+    console.log("Escuchano por el puerto 8888")
     model.sequelize.sync().then(function(){
         console.log("sequelize")
     })
