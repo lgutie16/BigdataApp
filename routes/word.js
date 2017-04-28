47e9ad11f3428f92d3c6fc0b471bcf72dc19d0a6
lgutie16 <lgutie16.eafit.edu.co>
2017-04-28 15:07:27 -0500

    Arrow func

--- routes/word.js
+++ routes/word.js
@@ -3,13 +3,13 @@ var router  	= express.Router();
 var Lgutie16 	= require('../models').Lgutie16
 
 router.post('/', function(req, res) {
-	Lgutie16.createRecord(req.body).then((result)=>{
+	Lgutie16.createRecord(req.body).then(function(result){
 		res.send(result)
 	}) 
 });
 
 router.post('/find', function(req, res) {
-	Lgutie16.listRecords(req.body).then((result)=>{
+	Lgutie16.listRecords(req.body).then(function(result){
 		data = {}
 		data.result = result
 		res.render('../app/views/', data);
@@ -18,13 +18,15 @@ router.post('/find', function(req, res) {
 });
 
 router.put('/', function(req, res) {
-	Lgutie16.updateRecord(req.body).then((result)=>{
+	Lgutie16.updateRecord(req.body).then(function(result){
 		res.send(result)
 	}) 
 });
 
 router.delete('/', function(req, res) {
-	Lgutie16.deleteRecord(req.body) 
+	Lgutie16.deleteRecord(req.body).then(function(result){
+		res.send({listo: true})
+	}) 
 });
 
 module.exports = router;
